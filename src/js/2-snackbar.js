1806aedf9e4e75fe3e8fd9619d6f852eb8a3275f
IhorBut737 <igor.klad@gmail.com>
2025-02-18 00:42:22 +0200

    timer

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const delayInput = form.querySelector('input[name="delay"]');
+const radioButtons = form.querySelectorAll('input[name="state"]');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = Number(delayInput.value);
+  const state = Array.from(radioButtons).find(radio => radio.checked).value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
